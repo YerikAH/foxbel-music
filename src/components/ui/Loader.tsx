320b8f6dcfe86082d53b126c67c36f1704ef40dc
Harvey Yerik <yerik05vh@gmail.com>
2023-04-03 23:09:19 -0500

    dev: implement loader

---
+++ src/components/ui/Loader.tsx
@@ -0,0 +1,13 @@
+import '../../styles/ui/loader.css'
+const Loader = () => {
+  return (
+    <div className='container'>
+      <div className='shape shape-1'></div>
+      <div className='shape shape-2'></div>
+      <div className='shape shape-3'></div>
+      <div className='shape shape-4'></div>
+    </div>
+  )
+}
+
+export default Loader
