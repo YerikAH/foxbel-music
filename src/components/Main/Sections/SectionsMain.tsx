2fcd6979fdc8e60a1e4ca6d0906a83dcaef62953
Harvey Yerik <yerik05vh@gmail.com>
2023-04-07 12:56:41 -0500

    dev: udpate project

--- src/components/Main/Sections/SectionsMain.tsx
+++ src/components/Main/Sections/SectionsMain.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useRef, useState } from 'react'
+import { useContext, useEffect, useRef } from 'react'
 import * as style from '../../../styles/main'
 import MusicContext from '../../../context/musicContext'
 import FetchContext from '../../../context/fetchContext'
@@ -28,17 +28,9 @@ const SectionsMain = () => {
     const moveScroll = element.scrollTop
     const maxScroll = element.scrollHeight - element.clientHeight
     const condition = moveScroll === maxScroll || (moveScroll === 0 && maxScroll === 0)
-    // console.log('-cumpole')
     if (condition) {
       fetchContext.handleContext()
     }
-    // const fetchData = async () => {
-    //   while (condition) {
-    //     await new Promise(resolve => setTimeout(resolve, 1000));
-    //     console.log('nueva peticion')
-    //     fetchContext.handleContext();
-    //   }
-    // };
   }
 
   return (
